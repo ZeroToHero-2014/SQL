acc7b53bd4a874bfe2234778b3bceae505e501f7
marcels <narcel.soare@teamnet.ro>
2014-10-29 10:26:33 +0200

    d3 session1

--- day3.sql
+++ day3.sql
@@ -1,6 +1,28 @@
 
+declare @sir  nvarchar(max) = 'vasile'
+set @sir = '1234456789012342123123123213123123'
 
 
 
+select @sir = @sir + ' dasdasdasd'
+
+--elect @sir
+
+
+----loop sample
+declare @limit int = 10
+declare @counter int =1 ;
+
+while @counter<= @limit
+begin
+	print @counter 
+	if @counter = 2
+	begin
+		break 
+	end 
+	set @counter = @counter +1 
+
+	
+end
 
 
