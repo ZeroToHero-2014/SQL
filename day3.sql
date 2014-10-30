64106ea7c66629ea425d72828f6915d9caff66c8
marcels <narcel.soare@teamnet.ro>
2014-10-30 20:15:36 +0200

    modificari

--- day3.sql
+++ day3.sql
@@ -1,4 +1,4 @@
-
+1
 declare @sir  nvarchar(max) = 'vasile'
 set @sir = '1234456789012342123123123213123123'
 
@@ -25,4 +25,40 @@ begin
 	
 end
 
+
+-----variabila de tip tabel
+declare @tbl table ( id int primary key identity, firstname nvarchar(100))
+
+insert @tbl values ('gogog')
+
+select * from @tbl
+
+------
+
+
+----parcurgerea secventiala folosind cursor
+
+
+----parcurgerea secventiala folosind bucla while
+
+declare @tbl table ( id int primary key identity, firstname nvarchar(100))
+declare @id int = 1
+declare @maxId int 
+
+
+
+insert into @tbl values ('vasile1')
+insert into @tbl values ('vasile2')
+insert into @tbl values ('vasile3')
+
+set @maxId = (select max(id) from @tbl)
+
+while @id<= @maxId
+begin
+	
+	
+	select * from @tbl where id = @id	
+	
+	set @id = @id +1 
+end
 
